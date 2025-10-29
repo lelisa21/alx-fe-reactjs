545ebc04e0dd3d3cfc02c90b09b95e856bb586c9
Laloo <lelisahailu22@gmail.com>
2025-10-29 21:26:47 +0300

    initial commit

---
+++ alx-react-app/src/App.jsx
@@ -0,0 +1,23 @@
+
+import React from 'react'
+import WelcomeMessage from './components/WelcomeMessage'
+import Header from './Header'
+import MainContent from './MainContent'
+import Footer from './Footer'
+import UserProfile from './components/UserProfile'
+
+const App = () => {
+  return (
+    <div>
+        <WelcomeMessage />
+        <Header />
+        <MainContent />
+        <Footer />
+        <UserProfile name = "Laloo" age = {20} bio = "He is an Amazing Software Engineer" />
+        <UserProfile name = "Abel" age = {22} bio = "He is an Amazing Full Stack Developer!" />
+        <UserProfile name = "mikiyas" age = {24} bio = "He is an Amazing Frontend Developer!" />
+     </div>
+  )
+}
+
+export default App
