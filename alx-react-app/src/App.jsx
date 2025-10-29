7b72af10ce8e0acb96b2b3344703548a0b865b6c
Laloo <lelisahailu22@gmail.com>
2025-10-29 21:35:13 +0300

    I dooo

--- alx-react-app/src/App.jsx
+++ alx-react-app/src/App.jsx
@@ -9,10 +9,12 @@ import UserProfile from './components/UserProfile'
 const App = () => {
   return (
     <div>
-        <WelcomeMessage />
         <Header />
         <MainContent />
         <Footer />
+        
+        <WelcomeMessage />
+
         <UserProfile name = "Laloo" age = {20} bio = "He is an Amazing Software Engineer" />
         <UserProfile name = "Abel" age = {22} bio = "He is an Amazing Full Stack Developer!" />
         <UserProfile name = "mikiyas" age = {24} bio = "He is an Amazing Frontend Developer!" />
