171ce263bb889a92e94cfc1cbfa58d876af064d5
Laloo <lelisahailu22@gmail.com>
2025-11-08 17:23:08 +0300

    updated

--- alx-react-app-props/src/UserProfile.jsx
+++ alx-react-app-props/src/UserProfile.jsx
@@ -1,5 +1,5 @@
-import { useContext } from 'react';
-import  UserContext  from './UserContext';
+import { useContext } from "react";
+import UserContext from "./usercontext";
 
 function UserProfile() {
   const userData = useContext(UserContext);
