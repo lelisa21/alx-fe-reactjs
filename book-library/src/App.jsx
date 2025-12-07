890b1d295df329b14cc927c06a7528276a333eac
Laloo <lelisahailu22@gmail.com>
2025-12-07 20:25:54 +0300

    initial commit

---
+++ book-library/src/App.jsx
@@ -0,0 +1,12 @@
+import NavBar from "./components/NavBar"
+
+const App = () => {
+  return (
+    <div className='text-4xl min-h-screen'>
+        <h2> Book Library</h2>
+        <NavBar />
+       </div>
+  )
+}
+
+export default App
---
+++ book-library/src/components/NavBar.tsx
@@ -0,0 +1,16 @@
+
+const NavBar = () => {
+  return (
+<nav>
+ <div className="md:flex items-center gap-6">
+            <a href="#" className="text-sm font-medium hover:text-primary transition-colors">Home</a>
+            <a href="#" className="text-sm font-medium hover:text-primary transition-colors">Browse</a>
+            <a href="#" className="text-sm font-medium hover:text-primary transition-colors">My Books</a>
+            <a href="#" className="text-sm font-medium hover:text-primary transition-colors">Calendar</a>
+            <a href="#" className="text-sm font-medium hover:text-primary transition-colors">Help</a>
+          </div>
+</nav>
+  )
+}
+
+export default NavBar
