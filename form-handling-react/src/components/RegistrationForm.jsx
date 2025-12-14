f9bca558fc76548932f28fc35893023390f92e2b
Laloo <lelisahailu22@gmail.com>
2025-12-14 18:22:53 +0300

    fix: userName changed to username

--- form-handling-react/src/components/RegistrationForm.jsx
+++ form-handling-react/src/components/RegistrationForm.jsx
@@ -1,7 +1,7 @@
 import React, { useState } from "react";
 
 const RegistrationForm = () => {
-  const { userName, setuserName } = useState("");
+  const { username, setUsername } = useState("");
   const { email, setEmail } = useState("");
   const { password, setPassword } = useState("");
   const { error, setError } = useState("");
@@ -9,25 +9,25 @@ const RegistrationForm = () => {
   const handleSubmit = (e) => {
     e.preventDefault();
 
-    if (!userName || !password || !email) {
+    if (!username || !password || !email) {
       setError("All fields Are required!");
       return;
     }
     setError("");
-    console.log(userName, email, password);
+    console.log(username, email, password);
     alert("Registration Secussfully!");
   };
   return <form onSubmit={handleSubmit}>
    User Registration
    <label htmlFor="username">user name: </label>
    <input type="text" id="username" name = "username"
-   value={userName}
-   onChange={(e) => setuserName(e.target.value)}/> <br />
-   <label htmlFor="email">user name: </label>
+   value={username}
+   onChange={(e) => setUsername(e.target.value)}/> <br />
+   <label htmlFor="email">Email: </label>
    <input type="email" id="email" name = "email"
    value={email}
    onChange={e => setEmail(e.target.value)}/> <br />
-   <label htmlFor="password">user name: </label>
+   <label htmlFor="password">Password: </label>
    <input type="password" id="password" name = "password"
    value={password}
    onChange={e => setPassword(e.target.value)}
